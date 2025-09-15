2808b648b31d95ea7fe941cc57aaebc24f8e97aa
raj <raj@administrators-iMac.local>
2025-09-15 23:27:02 +0530

    Improve position

---
+++ dist/Positions.vue.d.ts
@@ -0,0 +1,11 @@
+import { Position } from '@y2kfund/core';
+import { PositionsProps } from './index';
+declare const _default: import('vue').DefineComponent<PositionsProps, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {
+    "row-click": (row: Position) => any;
+}, string, import('vue').PublicProps, Readonly<PositionsProps> & Readonly<{
+    "onRow-click"?: ((row: Position) => any) | undefined;
+}>, {
+    accountId: string;
+    highlightPnL: boolean;
+}, {}, {}, {}, string, import('vue').ComponentProvideOptions, false, {}, HTMLElement>;
+export default _default;
