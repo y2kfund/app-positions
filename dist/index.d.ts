f9f888b011949d1ee1f3964f6ee82bd2252ef88a
raj <raj@administrators-iMac.local>
2025-09-16 16:31:59 +0530

    Improve position

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+import { default as Positions } from './Positions.vue';
+export { Positions };
+export default Positions;
+export interface PositionsProps {
+    accountId: string;
+    highlightPnL?: boolean;
+    onRowClick?: (row: any) => void;
+}
